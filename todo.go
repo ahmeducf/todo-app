53d95a62bff74254e0b57b2d187f7803a85f49d9
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2022-07-28 11:17:02 +0200

    fix bad request bugs

--- todo.go
+++ todo.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"path"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"gorm.io/driver/sqlite"
@@ -11,23 +12,41 @@ import (
 )
 
 
-type Todo struct {
+type TodoItem struct {
 	ID uint			`json:"id"`
 	Title string	`json:"title"`
 	Completed bool	`json:"completed"`
 }
 
-var (
-	dbFilePath 	  = path.Join("database", "todo.db")
+const (
 	listenPort    = ":8080"
+	badRequest 	  = "Request is not valid\n"
 )
 
-var db *gorm.DB
-var router = mux.NewRouter()
+type TodoServer struct {
+	todoItem TodoItem
+	db *gorm.DB
+	dbFilePath string
+}
 
-func getAllTodos(w http.ResponseWriter, req *http.Request) {
-	var todos []Todo
-	if err := db.Find(&todos).Error ;err != nil {
+func New() *TodoServer {
+	return &TodoServer{todoItem: TodoItem{}, db: nil}
+}
+
+func (app *TodoServer) initDb() error {
+	db, err := gorm.Open(sqlite.Open(app.dbFilePath), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	db.AutoMigrate(&TodoItem{})
+	app.db = db
+
+	return err
+}
+
+func (app *TodoServer) GetAllTodos(w http.ResponseWriter, req *http.Request) {
+	var todos []TodoItem
+	if err := app.db.Find(&todos).Error ;err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -41,79 +60,54 @@ func getAllTodos(w http.ResponseWriter, req *http.Request) {
 	writeHttpResponse(w, http.StatusOK, data)
 }
 
-func addTodoItem(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", "application/json")
-
-	var todoItem Todo
+func (app *TodoServer) AddTodoItem(w http.ResponseWriter, req *http.Request) {
+	var todoItem TodoItem
 	if err := json.NewDecoder(req.Body).Decode(&todoItem); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	if todoItem.ID == 0 || len(todoItem.Title) == 0 {
+		http.Error(w, badRequest, http.StatusBadRequest)
+		return
+	}
 
-	if result := db.First(&Todo{}, todoItem.ID); result.Error == nil {
+	if result := app.db.First(&TodoItem{}, todoItem.ID); result.Error == nil {
 		w.WriteHeader(http.StatusConflict)
 		return 
 
-	} else if result := db.Create(&todoItem); result.Error != nil {
-		http.Error(w, result.Error.Error(), http.StatusConflict)
+	} else if result := app.db.Create(&todoItem); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
 		return 
 	}
 
-	w.WriteHeader(http.StatusCreated)
-}
-
-func getTodoItemById(w http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	
-	var item Todo
-	err := db.First(&item, vars["id"]).Error
+	data, err := json.Marshal(todoItem)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	} else {
-		data, err := json.Marshal(item)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		writeHttpResponse(w, http.StatusOK, data)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+	writeHttpResponse(w, http.StatusCreated, data)
 }
 
-func deleteTodoItemById(w http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-
-	var item Todo
-	err := db.First(&item, vars["id"]).Error
+func (app *TodoServer) UpdateTodoItem(w http.ResponseWriter, req *http.Request) {
+	var item TodoItem
+	var newItem TodoItem
+	err := json.NewDecoder(req.Body).Decode(&newItem)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	} else {
-		if err := db.Delete(item).Error; err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
-}
 
-func updateTodoItemById(w http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
+	if newItem.ID == 0 || len(newItem.Title) == 0 {
+		http.Error(w, badRequest, http.StatusBadRequest)
+		return
+	}
 
-	var item Todo
-	err := db.First(&item, vars["id"]).Error
+	err = app.db.First(&item, newItem.ID).Error
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	} else {
-		var newItem Todo
-		json.NewDecoder(req.Body).Decode(&newItem)
-
 		item.Title = newItem.Title
 		item.Completed = newItem.Completed
-
-		if err := db.Save(item).Error; err != nil {
+		if err := app.db.Save(item).Error; err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -128,25 +122,78 @@ func updateTodoItemById(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (app *TodoServer) GetTodoItemById(w http.ResponseWriter, req *http.Request) {
+	var id int
+	var err error
+	vars := mux.Vars(req)
+
+	if id, err = strconv.Atoi(vars["id"]); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var item TodoItem
+	err = app.db.First(&item, id).Error
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	} else {
+		data, err := json.Marshal(item)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		writeHttpResponse(w, http.StatusOK, data)
+	}
+}
+
+func (app *TodoServer) DeleteTodoItemById(w http.ResponseWriter, req *http.Request) {
+	var id int
+	var err error
+	vars := mux.Vars(req)
+	if id, err = strconv.Atoi(vars["id"]); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var item TodoItem
+	err = app.db.First(&item, id).Error
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	} else {
+		if err := app.db.Delete(item).Error; err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
 func writeHttpResponse(w http.ResponseWriter, statusCode int, data []byte) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	w.Write(data)
 }
 
-func RegisterTodoRoutes() {
-	router.HandleFunc("/todos", getAllTodos).Methods("GET")
-	router.HandleFunc("/todos", addTodoItem).Methods("POST")
-	router.HandleFunc("/todos/{id}", getTodoItemById).Methods("GET")
-	router.HandleFunc("/todos/{id}", deleteTodoItemById).Methods("DELETE")
-	router.HandleFunc("/todos/{id}", updateTodoItemById).Methods("PATCH")
+func (app *TodoServer)registerTodoRoutes(router *mux.Router) {
+	router.HandleFunc("/todos", app.GetAllTodos).Methods("GET")
+	router.HandleFunc("/todos", app.AddTodoItem).Methods("POST")
+	router.HandleFunc("/todos", app.UpdateTodoItem).Methods("PATCH")
+	router.HandleFunc("/todos/{id}", app.GetTodoItemById).Methods("GET")
+	router.HandleFunc("/todos/{id}", app.DeleteTodoItemById).Methods("DELETE")
 }
 
 func main() {
-	db, _ = gorm.Open(sqlite.Open(dbFilePath), &gorm.Config{})
-	db.AutoMigrate(&Todo{})
+	app := New()
+	app.dbFilePath = path.Join("database", "todo.db")
+	err := app.initDb()
+	if err != nil {
+		panic(err)
+	}
 
-
-	RegisterTodoRoutes()
+	var router = mux.NewRouter()
+	app.registerTodoRoutes(router)
 	http.ListenAndServe(listenPort, router)
 }
